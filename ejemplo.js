aa516872f3c69570603bfcc9b7bc3e30350b9ba8
carconex <karmeconemar@gmail.com>
2023-11-09 18:44:18 +0100

    archivos que tiene que subir usu2

---
+++ ejemplo.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Que se puede hacer con html?</h2>
+
+<p id="demo">Con la ayuda de JS podemos cambiar su contenido</p>
+
+<button type="button" onclick='document.getElementById("demo").innerHTML = "Hello JavaScript!"'>Clica!</button>
+
+</body>
+</html>
